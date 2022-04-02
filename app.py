d505338f7fa2ba5903ea4928ebdf10d68f9c26a7
Larss_J <larss.jakobsons@icloud.com>
2022-04-02 18:41:14 +0300

    presence update

--- app.py
+++ app.py
@@ -16,9 +16,11 @@ from discord.ext.tasks import loop
 load_dotenv()
 bot = commands.Bot(command_prefix='!')
 
+
 @bot.event
 async def on_ready():
     print(f"{bot.user} has connected to Discord!")
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="the chat"))
 
 colours = [discord.Colour(0xe91e63), discord.Colour(0x0000FF0), discord.Colour(0x00FF00), discord.Colour(0xFF0000)]
 guild_id = 12345
@@ -38,144 +40,149 @@ async def colour_change_before():
 
 colour_change.start()
 
-player1 = ""
-player2 = ""
-turn = ""
-gameOver = True
+@bot.command(name='spam', help='Spams the input message for x number of times')
+async def spam(ctx, amount:int, *, message):
+    for i in range(amount): # Do the next thing amount times
+        await ctx.send(message) # Sends message where command was called
 
-board = []
+def tictactoe():
+    player1 = ""
+    player2 = ""
+    turn = ""
+    gameOver = True
 
-winningConditions = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-]
+    board = []
 
-@bot.command()
-async def tictactoe(ctx, p1: discord.Member, p2: discord.Member):
-    global count
-    global player1
-    global player2
-    global turn
-    global gameOver
+    winningConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
 
-    if gameOver:
-        global board
-        board = [":white_large_square:", ":white_large_square:", ":white_large_square:",
-                 ":white_large_square:", ":white_large_square:", ":white_large_square:",
-                 ":white_large_square:", ":white_large_square:", ":white_large_square:"]
-        turn = ""
-        gameOver = False
-        count = 0
+    @bot.command()
+    async def tictactoe(ctx, p1: discord.Member, p2: discord.Member):
+        global count
+        global player1
+        global player2
+        global turn
+        global gameOver
 
-        player1 = p1
-        player2 = p2
+        if gameOver:
+            global board
+            board = [":white_large_square:", ":white_large_square:", ":white_large_square:",
+                    ":white_large_square:", ":white_large_square:", ":white_large_square:",
+                    ":white_large_square:", ":white_large_square:", ":white_large_square:"]
+            turn = ""
+            gameOver = False
+            count = 0
 
-        # print the board
-        line = ""
-        for x in range(len(board)):
-            if x == 2 or x == 5 or x == 8:
-                line += " " + board[x]
-                await ctx.send(line)
-                line = ""
-            else:
-                line += " " + board[x]
+            player1 = p1
+            player2 = p2
 
-        # determine who goes first
-        num = random.randint(1, 2)
-        if num == 1:
-            turn = player1
-            await ctx.send("It is <@" + str(player1.id) + ">'s turn.")
-        elif num == 2:
-            turn = player2
-            await ctx.send("It is <@" + str(player2.id) + ">'s turn.")
-    else:
-        await ctx.send("A game is already in progress! Finish it before starting a new one.")
+            # print the board
+            line = ""
+            for x in range(len(board)):
+                if x == 2 or x == 5 or x == 8:
+                    line += " " + board[x]
+                    await ctx.send(line)
+                    line = ""
+                else:
+                    line += " " + board[x]
 
-@bot.command()
-async def place(ctx, pos: int):
-    global turn
-    global player1
-    global player2
-    global board
-    global count
-    global gameOver
-
-    if not gameOver:
-        mark = ""
-        if turn == ctx.author:
-            if turn == player1:
-                mark = ":regional_indicator_x:"
-            elif turn == player2:
-                mark = ":o2:"
-            if 0 < pos < 10 and board[pos - 1] == ":white_large_square:" :
-                board[pos - 1] = mark
-                count += 1
-
-                # print the board
-                line = ""
-                for x in range(len(board)):
-                    if x == 2 or x == 5 or x == 8:
-                        line += " " + board[x]
-                        await ctx.send(line)
-                        line = ""
-                    else:
-                        line += " " + board[x]
-
-                checkWinner(winningConditions, mark)
-                print(count)
-                if gameOver == True:
-                    await ctx.send(mark + " wins!")
-                elif count >= 9:
-                    gameOver = True
-                    await ctx.send("It's a tie!")
-
-                # switch turns
-                if turn == player1:
-                    turn = player2
-                elif turn == player2:
-                    turn = player1
-            else:
-                await ctx.send("Be sure to choose an integer between 1 and 9 (inclusive) and an unmarked tile.")
+            # determine who goes first
+            num = random.randint(1, 2)
+            if num == 1:
+                turn = player1
+                await ctx.send("It is <@" + str(player1.id) + ">'s turn.")
+            elif num == 2:
+                turn = player2
+                await ctx.send("It is <@" + str(player2.id) + ">'s turn.")
         else:
-            await ctx.send("It is not your turn.")
-    else:
-        await ctx.send("Please start a new game using the !tictactoe command.")
+            await ctx.send("A game is already in progress! Finish it before starting a new one.")
+
+    @bot.command()
+    async def place(ctx, pos: int):
+        global turn
+        global player1
+        global player2
+        global board
+        global count
+        global gameOver
+
+        if not gameOver:
+            mark = ""
+            if turn == ctx.author:
+                if turn == player1:
+                    mark = ":regional_indicator_x:"
+                elif turn == player2:
+                    mark = ":o2:"
+                if 0 < pos < 10 and board[pos - 1] == ":white_large_square:" :
+                    board[pos - 1] = mark
+                    count += 1
+
+                    # print the board
+                    line = ""
+                    for x in range(len(board)):
+                        if x == 2 or x == 5 or x == 8:
+                            line += " " + board[x]
+                            await ctx.send(line)
+                            line = ""
+                        else:
+                            line += " " + board[x]
+
+                    checkWinner(winningConditions, mark)
+                    print(count)
+                    if gameOver == True:
+                        await ctx.send(mark + " wins!")
+                    elif count >= 9:
+                        gameOver = True
+                        await ctx.send("It's a tie!")
+
+                    # switch turns
+                    if turn == player1:
+                        turn = player2
+                    elif turn == player2:
+                        turn = player1
+                else:
+                    await ctx.send("Be sure to choose an integer between 1 and 9 (inclusive) and an unmarked tile.")
+            else:
+                await ctx.send("It is not your turn.")
+        else:
+            await ctx.send("Please start a new game using the !tictactoe command.")
 
 
-def checkWinner(winningConditions, mark):
-    global gameOver
-    for condition in winningConditions:
-        if board[condition[0]] == mark and board[condition[1]] == mark and board[condition[2]] == mark:
-            gameOver = True
+    def checkWinner(winningConditions, mark):
+        global gameOver
+        for condition in winningConditions:
+            if board[condition[0]] == mark and board[condition[1]] == mark and board[condition[2]] == mark:
+                gameOver = True
 
-@tictactoe.error
-async def tictactoe_error(ctx, error):
-    print(error)
-    if isinstance(error, commands.MissingRequiredArgument):
-        await ctx.send("Please mention 2 players for this command.")
-    elif isinstance(error, commands.BadArgument):
-        await ctx.send("Please make sure to mention/ping players (ie. <@688534433879556134>).")
+    @tictactoe.error
+    async def tictactoe_error(ctx, error):
+        print(error)
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send("Please mention 2 players for this command.")
+        elif isinstance(error, commands.BadArgument):
+            await ctx.send("Please make sure to mention/ping players (ie. <@688534433879556134>).")
 
-@place.error
-async def place_error(ctx, error):
-    if isinstance(error, commands.MissingRequiredArgument):
-        await ctx.send("Please enter a position you would like to mark.")
-    elif isinstance(error, commands.BadArgument):
-        await ctx.send("Please make sure to enter an integer.")
+    @place.error
+    async def place_error(ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send("Please enter a position you would like to mark.")
+        elif isinstance(error, commands.BadArgument):
+            await ctx.send("Please make sure to enter an integer.")
 
 with open("data/namedays.json", encoding="utf-8") as f:
     namedays = json.load(f)
 
 with open("data/namedays.json", encoding="utf-8") as f:
     namedays_ext = json.load(f)  
-      
-  
+     
 @bot.event
 async def on_message(message):
 
