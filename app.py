2d32784f721046744c241c3618d9fe31b83df30f
Larss_J <larss.jakobsons@icloud.com>
2022-04-09 21:35:27 +0300

    Welcome message addon

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import asyncio
+from unicodedata import name
 from discord.ext import commands, tasks
 import random
+from discord import utils
 import os
 from dotenv import load_dotenv
 import re
@@ -15,19 +17,30 @@ from random import choice
 from discord.utils import get
 from discord.ext.tasks import loop
 
-
 load_dotenv()
-bot = commands.Bot(command_prefix='!')
+intents = discord.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 playingStatus = ['Bloons TD 6', 'Celeste', 'Cuphead', "Five nights at Freddy's", 'Just shapes and beats', 'Minecraft', 'Krunker', 'osu!', 'Rocket Leauge', 'Fortnite','Chess']
 watchingStatus = ['Youtube', 'Twitch', 'the stock market', 'birds', 'Anime']
-listeningStatus = ['https://open.spotify.com/track/0gYqQKXYJ2LDyx07ie3V5j?si=7102696b6eba4135','https://open.spotify.com/track/1sdxkS2JHc7XSEWui1JqFB?si=76e1c6f4462f42d9','https://open.spotify.com/track/2WYs5LxOZfEyURXu7V0O1a?si=1f04ae799cf445b0','https://open.spotify.com/track/0rNIEBlBuo5108lnHyGbqJ?si=9cb9271884754842','https://open.spotify.com/track/7KVBqGLGhrEejVokzYd8vF?si=5bb5bf01669841c8']
+
+main_channel = None
 
 @bot.event
 async def on_ready():
     print(f"{bot.user} has connected to Discord!")
+    global main_channel
+    global main_channel_ezic
+    chans = bot.get_all_channels()
+    
+    for c in chans:
+        if c.name == "general":
+            main_channel = c
+            print(f"main channel is {main_channel.name}, id={main_channel.id}")
+            break
     while True:
-        statusType = random.randint(0, 2)
+        statusType = random.randint(0, 1)
         if statusType == 0:
             statusNum = random.randint(0, 10)
             await bot.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.playing, name=playingStatus[statusNum]))
@@ -38,152 +51,24 @@ async def on_ready():
             await bot.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.watching, name=watchingStatus[statusNum]))
             print(f'{bot.user} is now watching {watchingStatus[statusNum]}')
             await asyncio.sleep(600)
-        else:
-            statusNum = random.randint(0, 4)
-            await bot.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.listening, name=listeningStatus[statusNum]))
-            print(f'{bot.user} is now listening to {listeningStatus[statusNum]}')
-            await asyncio.sleep(240)
-        
-    
-
-def tictactoe():
-    player1 = ""
-    player2 = ""
-    turn = ""
-    gameOver = True
-
-    board = []
-
-    winningConditions = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
-    ]
-
-    @bot.command()
-    async def tictactoe(ctx, p1: discord.Member, p2: discord.Member):
-        global count
-        global player1
-        global player2
-        global turn
-        global gameOver
-
-        if gameOver:
-            global board
-            board = [":white_large_square:", ":white_large_square:", ":white_large_square:",
-                    ":white_large_square:", ":white_large_square:", ":white_large_square:",
-                    ":white_large_square:", ":white_large_square:", ":white_large_square:"]
-            turn = ""
-            gameOver = False
-            count = 0
-
-            player1 = p1
-            player2 = p2
-
-            # print the board
-            line = ""
-            for x in range(len(board)):
-                if x == 2 or x == 5 or x == 8:
-                    line += " " + board[x]
-                    await ctx.send(line)
-                    line = ""
-                else:
-                    line += " " + board[x]
-
-            # determine who goes first
-            num = random.randint(1, 2)
-            if num == 1:
-                turn = player1
-                await ctx.send("It is <@" + str(player1.id) + ">'s turn.")
-            elif num == 2:
-                turn = player2
-                await ctx.send("It is <@" + str(player2.id) + ">'s turn.")
-        else:
-            await ctx.send("A game is already in progress! Finish it before starting a new one.")
-
-    @bot.command()
-    async def place(ctx, pos: int):
-        global turn
-        global player1
-        global player2
-        global board
-        global count
-        global gameOver
-
-        if not gameOver:
-            mark = ""
-            if turn == ctx.author:
-                if turn == player1:
-                    mark = ":regional_indicator_x:"
-                elif turn == player2:
-                    mark = ":o2:"
-                if 0 < pos < 10 and board[pos - 1] == ":white_large_square:" :
-                    board[pos - 1] = mark
-                    count += 1
-
-                    # print the board
-                    line = ""
-                    for x in range(len(board)):
-                        if x == 2 or x == 5 or x == 8:
-                            line += " " + board[x]
-                            await ctx.send(line)
-                            line = ""
-                        else:
-                            line += " " + board[x]
-
-                    checkWinner(winningConditions, mark)
-                    print(count)
-                    if gameOver == True:
-                        await ctx.send(mark + " wins!")
-                    elif count >= 9:
-                        gameOver = True
-                        await ctx.send("It's a tie!")
-
-                    # switch turns
-                    if turn == player1:
-                        turn = player2
-                    elif turn == player2:
-                        turn = player1
-                else:
-                    await ctx.send("Be sure to choose an integer between 1 and 9 (inclusive) and an unmarked tile.")
-            else:
-                await ctx.send("It is not your turn.")
-        else:
-            await ctx.send("Please start a new game using the !tictactoe command.")
-
-
-    def checkWinner(winningConditions, mark):
-        global gameOver
-        for condition in winningConditions:
-            if board[condition[0]] == mark and board[condition[1]] == mark and board[condition[2]] == mark:
-                gameOver = True
-
-    @tictactoe.error
-    async def tictactoe_error(ctx, error):
-        print(error)
-        if isinstance(error, commands.MissingRequiredArgument):
-            await ctx.send("Please mention 2 players for this command.")
-        elif isinstance(error, commands.BadArgument):
-            await ctx.send("Please make sure to mention/ping players (ie. <@688534433879556134>).")
-
-    @place.error
-    async def place_error(ctx, error):
-        if isinstance(error, commands.MissingRequiredArgument):
-            await ctx.send("Please enter a position you would like to mark.")
-        elif isinstance(error, commands.BadArgument):
-            await ctx.send("Please make sure to enter an integer.")
 
 with open("data/namedays.json", encoding="utf-8") as f:
     namedays = json.load(f)
 
 with open("data/namedays.json", encoding="utf-8") as f:
     namedays_ext = json.load(f)  
-     
+
+@bot.event
+async def on_member_join(member):
+    print("Recognised that a member called " + member.name + " joined")
+    await main_channel.send(f'''
+Hey {member}! Im glad you're here!
+Welcome to the gaming server!
+To access chatting wait 10min so we can verify you.
+Have fun chatting and being a part of this community!        
+    ''')
+    print("Sent message to " + member.name)
+
 @bot.event
 async def on_message(message):
 
@@ -221,35 +106,40 @@ async def on_message(message):
         await channel.send(msg)
 
     elif message.content.startswith('$'):
-      emojiup = '✅'
-      emojidown = '❌'
-      emoji1 = '1️⃣'
-      emoji2 = '2️⃣'
-      emoji3 = '3️⃣'
-      emoji4 = '4️⃣'
-      emoji5 = '5️⃣'
-      if message.content.startswith('$ '):
-        await message.add_reaction(emojiup)
-        await message.add_reaction(emojidown)
-      elif message.content.startswith('$2'):
-        await message.add_reaction(emoji1)
-        await message.add_reaction(emoji2)
-      elif message.content.startswith('$3'):
-        await message.add_reaction(emoji1)
-        await message.add_reaction(emoji2)
-        await message.add_reaction(emoji3)
-      elif message.content.startswith('$4'):
-        await message.add_reaction(emoji1)
-        await message.add_reaction(emoji2)
-        await message.add_reaction(emoji3)
-        await message.add_reaction(emoji4)
-      elif message.content.startswith('$5'):
-        await message.add_reaction(emoji1)
-        await message.add_reaction(emoji2)
-        await message.add_reaction(emoji3)
-        await message.add_reaction(emoji4)
-        await message.add_reaction(emoji5)
+        emojiup = '✅'
+        emojidown = '❌'
+        emoji1 = '1️⃣'
+        emoji2 = '2️⃣'
+        emoji3 = '3️⃣'
+        emoji4 = '4️⃣'
+        emoji5 = '5️⃣'
+        if message.content.startswith('$ '):
+            await message.add_reaction(emojiup)
+            await message.add_reaction(emojidown)
+        elif message.content.startswith('$2 '):
+            await message.add_reaction(emoji1)
+            await message.add_reaction(emoji2)
+        elif message.content.startswith('$3 '):
+            await message.add_reaction(emoji1)
+            await message.add_reaction(emoji2)
+            await message.add_reaction(emoji3)
+        elif message.content.startswith('$4 '):
+            await message.add_reaction(emoji1)
+            await message.add_reaction(emoji2)
+            await message.add_reaction(emoji3)
+            await message.add_reaction(emoji4)
+        elif message.content.startswith('$5 '):
+            await message.add_reaction(emoji1)
+            await message.add_reaction(emoji2)
+            await message.add_reaction(emoji3)
+            await message.add_reaction(emoji4)
+            await message.add_reaction(emoji5)
+        else:
+            await message.reply('The vote option count must be < 2≤X≤5 > !')
 
+
+intents = discord.Intents.default()
+intents.members = True
 #keep_alive()
 secret_TOKEN = os.environ['CUSTOMCONNSTR_DISCORD_TOKEN']
 bot.run(secret_TOKEN)   
