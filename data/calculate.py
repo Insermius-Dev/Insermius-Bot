9cea7f90bab9df9cd91a066fe59c5f715b0c18a1
Larss_J <larss.jakobsons@icloud.com>
2024-04-14 20:11:06 +0300

    Refactor Calculate class in calculate.py and Clear class in clear.py

--- data/calculate.py
+++ data/calculate.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from const import DELETE_BTN
 
 
-class Calculate(Extension) : 
+class Calculate(Extension):
 
     @slash_command(name="calculate", description="calculate some numbers")
     @slash_option(
@@ -13,14 +13,19 @@ class Calculate(Extension) :
         opt_type=OptionType.STRING,
         description="input your math equation",
     )
-    async def calc(self, ctx, equation):
+    async def calc(self, ctx, equation) -> None:
+        # using a library made by zevaryx
         try:
             answer = calculate(equation)
+
             embed = Embed(
                 title="Calculator",
                 color=Color.from_rgb(52, 152, 219),
-                timestamp=datetime.utcnow(),
-                footer=EmbedFooter(text=f"Requested by {ctx.author.display_name}", icon_url=ctx.author.avatar.url)
+                timestamp=datetime.now(datetime.UTC),
+                footer=EmbedFooter(
+                    text=f"Requested by {ctx.author.display_name}",
+                    icon_url=ctx.author.avatar.url,
+                ),
             )
             embed.add_field(name="Expression", value=f"`{equation}`")
             embed.add_field(name="Result", value=f"{answer}")
--- data/clear.py
+++ data/clear.py
@@ -1,7 +1,8 @@
-from interactions import * 
+from interactions import *
 
-class Clear(Extension) : 
-        
+
+class Clear(Extension):
+
     @slash_command(
         name="clear",
         description="clears messages",
@@ -27,36 +28,49 @@ class Clear(Extension) :
         required=False,
         opt_type=OptionType.BOOLEAN,
     )
-    async def clear(self, ctx, count, user=None, bot=False):
+    async def clear(self, ctx, count, user=None, bot=False) -> None:
         reason = f"@{ctx.author.display_name}({ctx.author.id}) cleared {count} messages"
-        if user:
-            if bot:
-                if user.bot:
-                    await ctx.channel.purge(
-                        deletion_limit=count,
-                        predicate=lambda m: m.author == user,
-                        reason=reason,
-                    )
-                else:
-                    count = 0
-            else:
-                await ctx.channel.purge(
-                    deletion_limit=count,
-                    predicate=lambda m: m.author == user,
-                    reason=reason,
-                )
-        elif bot:
+
+        target = user if ((user and bot) and user.bot) else False if ((user and bot) and not user.bot) else user if user else user.bot if bot else True
+
+        if target:
+            count = 0
+        elif not target:
+            await ctx.channel.purge(deletion_limit=count);
+        else:
             await ctx.channel.purge(
                 deletion_limit=count,
-                predicate=lambda m: m.author == user.bot,
+                predicate=lambda m: m.author == target,
                 reason=reason,
             )
-        if (user == None) and (bot == False):
-            await ctx.channel.purge(deletion_limit=count)
-        if user == None:
-            user = "`All`"
-        elif user:
-            user = user.mention
+
+        # if user and bot:
+        #     if user.bot:
+        #         await ctx.channel.purge(
+        #             deletion_limit=count,
+        #             predicate=lambda m: m.author == user,
+        #             reason=reason,
+        #         )
+        #     else:
+        #         count = 0
+
+        # elif user:
+        #     await ctx.channel.purge(
+        #         deletion_limit=count,
+        #         predicate=lambda m: m.author == user,
+        #         reason=reason,
+        #     )
+        # elif bot:
+        #     await ctx.channel.purge(
+        #         deletion_limit=count,
+        #         predicate=lambda m: m.author == user.bot,
+        #         reason=reason,
+        #     )
+        # if (user == None) and (bot == False):
+        #     await ctx.channel.purge(deletion_limit=count)
+
+        user = "`All`" if user == None else user.displaly_name
+
         embed = Embed(
             title="Messages deleted successfully",
             description=f"> `{count}` messages deleted succesfully",
