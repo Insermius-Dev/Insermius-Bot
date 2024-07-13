3f0967ef5a705c6779cd373d0d6ac7e22b73a222
Pre1ude0 <larss.jakobsons@icloud.com>
2024-07-14 00:49:29 +0300

    Added stars

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,24 @@
+function plantStars() {
+    for (let i = 0; i < 100; i++) {
+        const star = document.createElement('div');
+        star.classList.add('dot');
+
+        star.style.left = `${Math.random() * 100}vw`;
+        let height = document.body.scrollHeight;
+        star.style.top = `${Math.random() * height}px`;
+        star.style.width = `${Math.random() * 3}px`;
+        star.style.animationDuration = `${Math.random() * 2 + 3}s`;
+        star.style.animationDelay = `${Math.random() * 5 + 2}s`;
+        document.body.appendChild(star);
+    }
+
+    // play the animation
+    document.body.classList.add('fade-in-stars');
+}
+
 document.addEventListener("DOMContentLoaded", function() {
+    plantStars();
+
     const star = document.querySelector('.star');
 
     // Remove initial animation class after it completes
@@ -17,4 +37,5 @@ document.addEventListener("DOMContentLoaded", function() {
         // Add click-animation class
         star.classList.add('click-animation');
     });
+
 });
